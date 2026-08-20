dd45778335155833a1f6ae870d5f6e0a9723acd1
Avik Das <avikd957@gmail.com>
2026-08-20 16:18:05 +0530

    Step 21: tile_rowsum

--- model.py
+++ model.py
@@ -335,8 +335,20 @@ __device__ void tile_exp(float* s_tile, const float* row_max,
     }
 }
 
-# Step 21 - tile_rowsum (not yet solved)
-# TODO: implement
+# Step 21 - tile_rowsum
+__device__ void tile_rowsum(const float* p_tile, float* row_sum_out,
+                            int tile_q, int tile_k,
+                            int thread_id, int num_threads) {
+    for (int r = thread_id; r < tile_q; r += num_threads) {
+        float sum = 0.0f;
+
+        for (int c = 0; c < tile_k; ++c) {
+            sum += p_tile[r * tile_k + c];
+        }
+
+        row_sum_out[r] = sum;
+    }
+}
 
 # Step 22 - accumulate_pv (not yet solved)
 # TODO: implement

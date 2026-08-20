dc30e341267f7c9aa9296fd0be5ee8c52a7bd5c6
Avik Das <avikd957@gmail.com>
2026-08-20 16:10:30 +0530

    Step 13: online_max

--- model.py
+++ model.py
@@ -241,8 +241,10 @@ void naive_attention(const float* d_q, const float* d_k, const float* d_v,
     cudaFree(d_scores);
 }
 
-# Step 13 - online_max (not yet solved)
-# TODO: implement
+# Step 13 - online_max
+__device__ float online_max(float old_max, float new_val) {
+    return fmaxf(old_max, new_val);
+}
 
 # Step 14 - correction_factor (not yet solved)
 # TODO: implement
